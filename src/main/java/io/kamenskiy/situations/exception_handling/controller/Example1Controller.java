c57a2328a16534033ac8e846dd2f97187959f162
AndreyJavaEdu <scream123456@yandex.ru>
2024-02-20 22:47:36 +0300

    Создали dto для ответа Response в виде JSON. И реализовали метод в контроллере Example1Controller.java testExceptionExceptionHandler() - метод вернёт либо исключение BusinessException либо ответ Response.

--- src/main/java/io/kamenskiy/situations/exception_handling/controller/Example1Controller.java
+++ src/main/java/io/kamenskiy/situations/exception_handling/controller/Example1Controller.java
@@ -1,6 +1,8 @@
 package io.kamenskiy.situations.exception_handling.controller;
 
-import org.apache.catalina.connector.Response;
+import io.kamenskiy.situations.exception_handling.dto.Response;
+import io.kamenskiy.situations.exception_handling.exception.BusinessException;
+
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,9 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Example1Controller {
     @GetMapping(value = "/testExceptionHandler", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Response testExceptionExceptionHandler(@RequestParam(required = false, defaultValue = "false") boolean exception) {
-
-
-
+    public Response testExceptionExceptionHandler(@RequestParam(required = false, defaultValue = "false") boolean exception)
+            throws BusinessException {
+        if(exception){
+            throw new BusinessException("BusinessException in testExceptionExceptionHandler");
+        }
+        return new Response("Ok");
     }
+
+
 }
