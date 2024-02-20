0e5ce50f8818de5994da00f90ce27a509e1fb71a
AndreyJavaEdu <scream123456@yandex.ru>
2024-02-21 00:11:43 +0300

    Для примера создан класс исключения ServiceException унаследованный от Exception. Проаннотировали его @ResponseStatus, тем самым мы задействуем подкласс ResponseStatusExceptionResolver, который позволяет настроить код ответа для любого исключения.

---
+++ src/main/java/io/kamenskiy/situations/exception_handling/exception/ServiceException.java
@@ -0,0 +1,14 @@
+package io.kamenskiy.situations.exception_handling.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR) //будет возвращаться статус-код 500
+public class ServiceException extends Exception{
+    public ServiceException() {
+    }
+
+    public ServiceException(String message) {
+        super(message);
+    }
+}
