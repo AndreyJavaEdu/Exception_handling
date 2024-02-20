be82e180da68d6f535a375b3aafc886ddd147aaa
AndreyJavaEdu <scream123456@yandex.ru>
2024-02-20 22:26:49 +0300

    Создали класс контроллера Example1Controller

---
+++ src/main/java/io/kamenskiy/situations/exception_handling/controller/Example1Controller.java
@@ -0,0 +1,8 @@
+package io.kamenskiy.situations.exception_handling.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Example1Controller {
+
+}
