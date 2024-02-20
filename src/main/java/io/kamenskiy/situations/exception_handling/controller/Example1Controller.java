2c966c88a4f4e10f1e1ac97493f7497fab97a0ab
AndreyJavaEdu <scream123456@yandex.ru>
2024-02-20 22:38:48 +0300

    Создали класс исключения BusinessException.java

--- src/main/java/io/kamenskiy/situations/exception_handling/controller/Example1Controller.java
+++ src/main/java/io/kamenskiy/situations/exception_handling/controller/Example1Controller.java
@@ -1,8 +1,17 @@
 package io.kamenskiy.situations.exception_handling.controller;
 
+import org.apache.catalina.connector.Response;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Example1Controller {
+    @GetMapping(value = "/testExceptionHandler", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Response testExceptionExceptionHandler(@RequestParam(required = false, defaultValue = "false") boolean exception) {
 
+
+
+    }
 }
