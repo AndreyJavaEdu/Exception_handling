5cceb23763b672728ae6cc68cb061edc1dfa4b59
AndreyJavaEdu <scream123456@yandex.ru>
2024-02-20 22:15:24 +0300

    first commit

---
+++ src/main/java/io/kamenskiy/situations/exception_handling/ExceptionHandlingApplication.java
@@ -0,0 +1,13 @@
+package io.kamenskiy.situations.exception_handling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExceptionHandlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExceptionHandlingApplication.class, args);
+	}
+
+}
