0aef250191003897f3ad34dcc26c664197d7341a
AndreyJavaEdu <scream123456@yandex.ru>
2024-02-21 03:41:06 +0300

    Создан контроллер Example6Controller с методом testResponseStatusException в котором используется выброс исключения  ResponseStatusException в параметрах которого мы можем установить статус код и он будет показан пользователю, но тело ответа также отсутствует. Подходит для простых ситуаций.

---
+++ src/main/java/io/kamenskiy/situations/exception_handling/controller/Example6Controller.java
@@ -0,0 +1,23 @@
+package io.kamenskiy.situations.exception_handling.controller;
+
+import io.kamenskiy.situations.exception_handling.dto.Response;
+import io.kamenskiy.situations.exception_handling.exception.BusinessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestController
+public class Example6Controller {
+
+    @GetMapping(value = "/testResponseStatusException", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Response testResponseStatusException(@RequestParam(required = false, defaultValue = "false") boolean exception)
+        throws BusinessException{
+        if (exception){
+            throw new ResponseStatusException(HttpStatus.I_AM_A_TEAPOT, "ResponseStatusException in testResponseStatusException");
+        }
+        return new Response("Все ОК!!!");
+    }
+}
